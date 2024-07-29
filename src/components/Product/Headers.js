b10f5a02be0c0f10a50895e4dd7b060a2d4be83d
hubham98 <kumarshubham10021998@gmail.com>
2024-07-29 12:35:25 +0530

    update

--- src/components/Product/Headers.js
+++ src/components/Product/Headers.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Headers = ({ cartCount, toggleCartModal }) => {
     const [loggedInUser, setLoggedInUser] = useState(null);
@@ -25,8 +25,8 @@ const Headers = ({ cartCount, toggleCartModal }) => {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand m-lg-2" href="#">SHOPLANE</a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                <Link className="navbar-brand m-lg-2" to="/">SHOPLANE</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -34,25 +34,33 @@ const Headers = ({ cartCount, toggleCartModal }) => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mx-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">HOME <span className="sr-only">(current)</span></a>
+                            <Link className="nav-link" to="/">HOME <span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">CLOTHINGS</a>
+                            <Link className="nav-link" to="#">CLOTHINGS</Link>
                         </li>
                     </ul>
                     <div className='text-end mx-5'>
-                        <a className="nav-link" href="#" onClick={toggleCartModal}><i className="fa fa-shopping-cart" style={{ fontSize: "20px" }}></i><span className='p-2 mb-2 text-warning'>{cartCount}</span></a>
+                        <button className="nav-link btn" onClick={toggleCartModal}>
+                            <i className="fa fa-shopping-cart" style={{ fontSize: "20px" }}></i>
+                            <span className='p-2 mb-2 text-warning'>{cartCount}</span>
+                        </button>
                     </div>
                     <div className='text-end'>
                         {loggedInUser ? (
                             <div className="nav-item dropdown">
-                                <a className="nav-link mx-3" href="#" onClick={handleProfileClick}><i className="fa fa-user-circle-o" style={{ fontSize: "20px" }}><span className="mx-2">{loggedInUser}</span></i></a>
+                                <button className="nav-link mx-3 btn" onClick={handleProfileClick}>
+                                    <i className="fa fa-user-circle-o" style={{ fontSize: "20px" }}></i>
+                                    <span className="mx-2">{loggedInUser}</span>
+                                </button>
                                 {showLogout && (
                                     <button className="btn btn-outline-danger mt-2" onClick={handleLogout}>Logout</button>
                                 )}
                             </div>
                         ) : (
-                            <a className="nav-link mx-3" href="/login"><i className="fa fa-user-circle-o" style={{ fontSize: "20px" }}></i></a>
+                            <Link className="nav-link mx-3" to="/login">
+                                <i className="fa fa-user-circle-o" style={{ fontSize: "20px" }}></i>
+                            </Link>
                         )}
                     </div>
                 </div>
